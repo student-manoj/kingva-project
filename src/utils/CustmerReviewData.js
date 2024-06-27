80769460e07a10e090f8105dee27f6c8f2467f0c
Student Manoj <manoj02biswas@gmail.con>
2024-06-27 11:47:11 +0530

    CustmerReviewData page edit

--- src/utils/CustmerReviewData.js
+++ src/utils/CustmerReviewData.js
@@ -1,31 +1,31 @@
-// import CustmerReviewImg from "../../public/ReviewImg.png";
-// import CustmerReviewImg1 from "../../public/ReviewImg1.png";
-// import CustmerReviewImg2 from "../../public/ReviewImg2.png";
-// import CustmerReviewImg3 from "../../public/ReviewImg3.png";
+import CustmerReviewImg from "../../public/ReviewImg.png";
+import CustmerReviewImg1 from "../../public/ReviewImg1.png";
+import CustmerReviewImg2 from "../../public/ReviewImg2.png";
+import CustmerReviewImg3 from "../../public/ReviewImg3.png";
 
 export const CustmerReviewData = [
-  // {
-  //   id: 1,
-  //   title: "Happy Customers",
-  //   img: CustmerReviewImg,
-  //   reviewpoint: "555",
-  // },
-  // {
-  //   id: 2,
-  //   title: "Projects Finished ",
-  //   img: CustmerReviewImg1,
-  //   reviewpoint: "958",
-  // },
-  // {
-  //   id: 3,
-  //   title: "Award Winning",
-  //   img: CustmerReviewImg2,
-  //   reviewpoint: "254",
-  // },
-  // {
-  //   id: 4,
-  //   title: "Years experience",
-  //   img: CustmerReviewImg3,
-  //   reviewpoint: "548",
-  // },
+  {
+    id: 1,
+    title: "Happy Customers",
+    img: CustmerReviewImg,
+    reviewpoint: "555",
+  },
+  {
+    id: 2,
+    title: "Projects Finished ",
+    img: CustmerReviewImg1,
+    reviewpoint: "958",
+  },
+  {
+    id: 3,
+    title: "Award Winning",
+    img: CustmerReviewImg2,
+    reviewpoint: "254",
+  },
+  {
+    id: 4,
+    title: "Years experience",
+    img: CustmerReviewImg3,
+    reviewpoint: "548",
+  },
 ];
